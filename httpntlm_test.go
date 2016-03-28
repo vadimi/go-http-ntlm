8b649f8acdd337cbf4db396a1e6f0b525ea0dba6
vadimi <vadim.ivanou@gmail.com>
2016-03-27 23:21:44 -0400

    Initial commit

---
+++ httpntlm_test.go
@@ -0,0 +1,72 @@
+package httpntlm
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/ThomsonReutersEikon/go-ntlm/ntlm"
+)
+
+const (
+	buildsAPIURL = "https://tfs.videa.tv/tfs/Videa/Videa/_apis/build/builds"
+)
+
+func Test_AuthenticationSuccess(t *testing.T) {
+	client := http.Client{
+		Transport: &NtlmTransport{
+			Domain:   "dt",
+			User:     "testuser",
+			Password: "fish",
+		},
+	}
+
+	session, _ := ntlm.CreateServerSession(ntlm.Version2, ntlm.ConnectionlessMode)
+	session.SetUserInfo("testuser", "fish", "dt")
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		h := r.Header.Get("Authorization")
+		if h == "" {
+			w.WriteHeader(401)
+			return
+		}
+
+		ntlmChallengeString := strings.Replace(h, "NTLM ", "", -1)
+		authenticateBytes, _ := decBase64(ntlmChallengeString)
+		auth, err := ntlm.ParseAuthenticateMessage(authenticateBytes, 2)
+		if err == nil {
+			err = session.ProcessAuthenticateMessage(auth)
+			if err != nil {
+				t.Errorf("Could not process authenticate message: %s\n", err)
+				return
+			}
+		} else {
+			challenge, _ := session.GenerateChallengeMessage()
+			w.Header().Add("WWW-Authenticate", "NTLM "+encBase64(challenge.Bytes()))
+			w.WriteHeader(401)
+		}
+	}))
+
+	defer ts.Close()
+	req, err := http.NewRequest("GET", ts.URL, strings.NewReader(""))
+	resp, err := client.Do(req)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,53 @@
+# go-http-ntlm
+go-http-ntlm is a Go package that contains NTLM transport (`http.RoundTripper` implementation) for `http.Client` to make NTLM auth protected http requests.
+
+It is based on [https://github.com/ThomsonReutersEikon/go-ntlm](https://github.com/ThomsonReutersEikon/go-ntlm) library.
+
+
+## Usage example
+
+```
+package main
+
+import (
+    "io/ioutil"
+    "log"
+    "net/http"
+    "strings"
+
+    "github.com/vadimi/go-http-ntlm"
+)
+
+func main() {
+
+    // configure http client
+    client := http.Client{
+        Transport: &httpntlm.NtlmTransport{
+            Domain:   "mydomain",
+            User:     "testuser",
+            Password: "fish",
+        },
+    }
+
+    req, err := http.NewRequest("GET", "http://server/ntlm-auth-resource", strings.NewReader(""))
+    resp, err := client.Do(req)
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer func() {
+        err := resp.Body.Close()
+        if err != nil {
+            log.Fatal(err)
+        }
+    }()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    log.Println(body)
+}
+```
