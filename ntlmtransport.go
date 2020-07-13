e2642fbd1807187bb85e86fcc686b57c48143f38
vadimi <vadim.ivanou@gmail.com>
2020-07-12 22:04:54 -0400

    Use RoundTripper interface, fixes #9
    This will reuse connection pooling mechanism and will not create tons of
    connections for every request

--- ntlmtransport.go
+++ ntlmtransport.go
@@ -1,24 +1,21 @@
 package httpntlm
 
 import (
-	"crypto/tls"
 	"errors"
 	"io"
 	"io/ioutil"
-	"net"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/vadimi/go-ntlm/ntlm"
 )
 
 // NtlmTransport is implementation of http.RoundTripper interface
 type NtlmTransport struct {
-	TLSClientConfig *tls.Config
-	Domain          string
-	User            string
-	Password        string
+	Domain   string
+	User     string
+	Password string
+	http.RoundTripper
 }
 
 // RoundTrip method send http request and tries to perform NTLM authentication
@@ -27,19 +24,10 @@ func (t NtlmTransport) RoundTrip(req *http.Request) (res *http.Response, err err
 	r, _ := http.NewRequest("GET", req.URL.String(), strings.NewReader(""))
 	r.Header.Add("Authorization", "NTLM "+encBase64(negotiate()))
 
-	client := http.Client{Transport: &http.Transport{
-		Proxy: http.ProxyFromEnvironment,
-		DialContext: (&net.Dialer{
-			Timeout:   30 * time.Second,
-			KeepAlive: 30 * time.Second,
-			DualStack: true,
-		}).DialContext,
-		MaxIdleConns:          100,
-		IdleConnTimeout:       90 * time.Second,
-		TLSHandshakeTimeout:   10 * time.Second,
-		ExpectContinueTimeout: 1 * time.Second,
-		TLSClientConfig:       t.TLSClientConfig,
-	}}
+	client := http.Client{}
+	if t.RoundTripper != nil {
+		client.Transport = t.RoundTripper
+	}
 
 	resp, err := client.Do(r)
 	if err != nil {
